e1b330eff2c460261dfd0fab565bdcea69ef4fa1
WIN8OHS50178\ohs50178 <daichi.seitokai@gmail.com>
2018-09-11 12:05:36 +0900

    追加：寿司を作成可能、変更：オブジェクトの管理体制

--- Assets/Scripts/Sushi/SushiBase.cs
+++ Assets/Scripts/Sushi/SushiBase.cs
@@ -36,12 +36,26 @@ public class SushiBase : MonoBehaviour {
 	}
 
 
-
 	// 寿司を作る
 	private void MakeSushiWithNetaAndSyari(GameObject _neta) {
 		// ネタをシャリの子供に設定
 		_neta.transform.parent = this.transform;
 
-		// 
+		// ネタの位置を調整
+		_neta.transform.localPosition = new Vector3(0, 0, 0.005f);
+		// ネタの回転を調整
+		_neta.transform.localRotation = Quaternion.identity;
+
+		// rigidbodyを再設定
+		Rigidbody rb = _neta.transform.GetComponent<Rigidbody>();
+		rb.isKinematic = true;
+		rb.useGravity = false;
+
+		// コライダを無効化
+		BoxCollider bc = _neta.transform.GetComponent<BoxCollider>();
+		bc.enabled = false;
+
+		// 自分を寿司に設定
+		this.transform.tag = "sushi";
 	}
 }
